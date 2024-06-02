afc7ce09020a1b08c255267db3fcc933f838fedd
fernando rosero <fsrosero6@soy.sena.edu.co>
2024-06-02 15:49:52 -0500

    Inicio de sesion incompleto

--- app/src/main/java/com/example/ahorra/LoginActivity.kt
+++ app/src/main/java/com/example/ahorra/LoginActivity.kt
@@ -3,15 +3,12 @@ package com.example.ahorra
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        supportActionBar?.hide() // Oculta la ActionBar de forma segura si no es nula
+        enableEdgeToEdge() // Habilita el borde a borde si estás usando AndroidX Activity
         setContentView(R.layout.activity_login)
-
-
     }
 }
