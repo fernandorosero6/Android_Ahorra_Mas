168adde72782871e1a8729f335953836b76b20b5
Natalia Manquillo <manquillonatalia04@gmail.com>
2024-06-06 13:04:49 -0500

    Contador principal-agregar contador (modificar)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,6 +29,10 @@ android {
             )
         }
     }
+
+    buildFeatures {
+        viewBinding=true
+    }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
@@ -66,6 +70,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
---
+++ app/src/main/java/com/example/ahorra/ContadorActivity.kt
@@ -0,0 +1,20 @@
+package com.example.ahorra
+
+import android.R
+import android.view.View
+import android.view.View.OnClickListener
+import android.widget.ListView
+import androidx.appcompat.app.AppCompatActivity
+
+
+class ContadorActivity:AppCompatActivity(), OnClickListener {
+
+
+
+    override fun onClick(p0: View?) {
+
+
+//modificar direccionanimiento de botón
+
+    }
+}
